2f5e78bb3fed2a7152d131b8485882957d08dae6
Arda Peker <a.ardapeker@gmail.com>
2025-07-22 15:37:23 +0300

    refactored

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def main():
   with st.sidebar:
     st.subheader("Your documents")
     pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-    # pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'")
+   
     if pdf_docs and st.button("Process"):
       with st.spinner("Processing"):
         # get pdf text
