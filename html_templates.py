d3726003138f821b40309ed093c2a8ad332af8ab
Arda Peker <a.ardapeker@gmail.com>
2025-07-22 13:26:07 +0300

    initial

---
+++ html_templates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://pngset.com/images/top-5-discord-bots-for-gamers-in-2020-tremenheere-sculpture-gardens-electronics-text-face-transparent-png-2532139.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://image.pngaaa.com/345/1582345-middle.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
